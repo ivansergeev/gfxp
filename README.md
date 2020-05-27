db59f479a9ee9a348a99da3d40cbdadcc39ead4c
Ivan Sergeev <ivansergeev@gmail.com>
2020-05-27 16:30:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# GFXP
+
+GFXP is a small library with a collection of templates for Playdate. Includes a visual editor for modern browsers (no internet required) for quickly creating and using patterns.
+
+- Draw something in the visual editor and see the finished template
+- Copy the template in any code format or download PNG
+- Easy! Simply! Magic!
+
+### Using GFXP Editor
+
+Open gfxp-editor.html from the 'Tools' folder in any modern browser.
+
+
+### Using GFXP lib
+
+Before you begin, you must import the lib into the source folder of your project.
+
+```sh
+import 'helpers/gfxp'
+```
+
+
+### Use cases GFXP lib
+
+The library includes 100 ready-made patterns, for the use of which it is enough to indicate their name.
+
+Option 1:
+
+```sh
+local gfxp = GFXP
+-- in the code, specified pattern name
+gfxp.set('gray')
+-- or the specified pattern table
+gfxp.set({0xE7, 0xDB, 0xBD, 0x7E, 0x7E, 0xBD, 0xDB, 0xE7})
+-- or the specified pattern name
+playdate.graphics.setPattern(gfxp.gray)
+```
+
+Option 2:
+
+```
+-- in the code, specified pattern name
+GFXP.set('gray')
+-- or the specified pattern table
+GFXP.set({0xE7, 0xDB, 0xBD, 0x7E, 0x7E, 0xBD, 0xDB, 0xE7})
+```
+
+Option 3:
+
+```
+	playdate.graphics.setPattern(GFXP.gray)	
+```
+
+
+
+### License
+----
+
+The MIT License (MIT)
+
---
+++ Source/main.lua
@@ -0,0 +1,94 @@
+--[[ 
+	Using GFXP
+	
+	Option 1:
+		local gfxp = GFXP
+		-- in the code, specified pattern name
+		gfxp.set('gray')
+		-- or the specified pattern table
+		gfxp.set({0xE7, 0xDB, 0xBD, 0x7E, 0x7E, 0xBD, 0xDB, 0xE7})
+		-- or the specified pattern name
+		playdate.graphics.setPattern(gfxp.gray)
+		
+	Option 2:
+		-- in the code, specified pattern name
+		GFXP.set('gray')
+		-- or the specified pattern table
+		GFXP.set({0xE7, 0xDB, 0xBD, 0x7E, 0x7E, 0xBD, 0xDB, 0xE7})
+	
+	Option 3:
+		-- the specified pattern name
+		playdate.graphics.setPattern(GFXP.gray)	
+	
+]]--
+
+
+import 'CoreLibs/graphics'
+import 'CoreLibs/sprites'
+import 'CoreLibs/ui'
+import 'CoreLibs/ui/gridview'
+import 'CoreLibs/utilities/printer'
+import 'helpers/gfxp'
+
+local gfx = playdate.graphics
+local gfxp = GFXP
+
+local patternviewHeight = 240
+local showLabel = true
+local patterns = {}
+local patternview
+
+function init()
+
+	for key, val in pairs(gfxp) do
+		if key ~= 'set' then 
+			table.insert(patterns, key)
+		end
+	end
+
+	table.sort(patterns, function (a,b) return a<b end)
+
+	patternview = playdate.ui.gridview.new(400, 240)
+	patternview:setNumberOfRows(#patterns)
+
+	function patternview:drawCell(section, row, column, selected, x, y, width, height)
+
+		gfxp.set(patterns[row])		
+		gfx.fillRect(x, y, width, 240)
+
+		if showLabel then
+			gfx.setColor(gfx.kColorWhite)
+			gfx.fillRoundRect(150, y+5, 100, 30, 10)
+			gfx.drawTextInRect(patterns[row], x, y+11, width, height+30, nil, "", kTextAlignment.center)
+		end
+	end
+end
+
+init()
+
+-- Buttons
+
+function playdate.AButtonUp()
+	showLabel = not showLabel
+end
+
+function playdate.upButtonUp()
+	patternview:selectPreviousRow(false)
+end
+
+function playdate.downButtonUp()
+	patternview:selectNextRow(false)
+end
+
+-- Update
+
+function playdate.update()
+	-- gfx.clear()
+	playdate.timer.updateTimers()
+	patternview:drawInRect(0, 0, 400, patternviewHeight)
+end
+
+-- Tips
+
+print('Press the A button to show/hide the pattern label.')
+print('Press the Up / Down buttons to navigate.')
